ffe115a2a96592987ca6a858a78e1aa4b494b4e5
albertmarzo <albertmarzovila@gmail.com>
2025-06-01 19:34:00 +0200

    this is the third commit. it contains the game with the player, the asteroids and the shooting. only thing remaining is to add incremental logic for asteroid splitting instead of killing if asteroid is too big

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,12 @@
-from circleshape import CircleShape
+from circleshape import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        pass
+        super().__init__(x, y, radius)
+    
+    def draw(self, screen):
+        color = (255, 255, 255) #white in RGB
+        pygame.draw.circle(screen, color, self.position, self.radius, width = 2)
+    
+    def update(self, dt):
+        self.position += (self.velocity*dt)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
+from shot import Shot
 
 def main():
     pygame.init()
@@ -15,10 +19,19 @@ def main():
     
     updatable =  pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
 
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
+    
     screen_color = (0, 0, 0)
 
     while(1):
@@ -26,7 +39,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        updatable.update(dt)   
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.check_collision(player) == True:
+                sys.exit("Game Over!")
+        
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.check_collision(shot) == True:
+                    asteroid.kill()
+                    shot.kill() 
 
         screen.fill(screen_color)
 
