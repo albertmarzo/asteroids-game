0950df9ec7d7d00387ff1e2feecf0eda6265190c
albertmarzo <albertmarzovila@gmail.com>
2025-06-01 13:56:17 +0200

    the second commit of the Asteroid game project. basic functionalities of player have been added. now working on gthe Asteroids remain

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable =  pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    screen_color = (0, 0, 0)
+
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+
+        updatable.update(dt)   
+
+        screen.fill(screen_color)
+
+        for thing in drawable:
+            thing.draw(screen)
+  
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
