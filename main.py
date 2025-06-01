8ccc6d9fa7f58eeb0b6b6c94673b9d0635328ae8
albertmarzo <albertmarzovila@gmail.com>
2025-06-01 20:05:15 +0200

    the last commit.game finished.future upgrades to come.

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.check_collision(shot) == True:
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill() 
 
         screen.fill(screen_color)
